271c228ac7c3a380a9103743b754624c99230707
sandy <sandydwi75@gmail.com>
2024-12-07 18:50:51 +0700

    feat: register user API

---
+++ src/application/web.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+import { publicRoute } from '../route/public-api';
+import { errorMiddleware } from '../middleware/error-middleware';
+
+export const web = express();
+web.use(express.json());
+web.use(publicRoute);
+web.use(errorMiddleware);
