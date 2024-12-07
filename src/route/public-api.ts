9cf17cd82fddc5aefe727f1c747395afa0f9cfb2
sandy <sandydwi75@gmail.com>
2024-12-07 19:07:00 +0700

    feat: login user

--- src/route/public-api.ts
+++ src/route/public-api.ts
@@ -3,3 +3,4 @@ import { UserController } from '../controller/user-controller';
 
 export const publicRoute = express.Router();
 publicRoute.post('/api/users', UserController.register);
+publicRoute.post('/api/users/login', UserController.login);
