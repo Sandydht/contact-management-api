61251bc2f0ef80221c55f17ff807b68e4388ef9f
sandy <sandydwi75@gmail.com>
2024-12-07 19:31:05 +0700

    feat: get user API

--- src/application/web.ts
+++ src/application/web.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { publicRoute } from '../route/public-api';
 import { errorMiddleware } from '../middleware/error-middleware';
+import { apiRouter } from '../route/api';
 
 export const web = express();
 web.use(express.json());
 web.use(publicRoute);
 web.use(errorMiddleware);
+web.use(apiRouter);
