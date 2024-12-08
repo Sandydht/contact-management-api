55d7eb2c8afc01c685397168b835ab7f7bf18481
Sandy Dwi Handoko Trapsilo <48269439+Sandydht@users.noreply.github.com>
2024-12-09 01:25:25 +0700

    Delete src/Interfaces/http/api directory

--- src/Interfaces/http/api/api.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import UserController from "./controllers/UserController";
-
-const router = express.Router();
-
-router.use('/api/users', UserController);
-
-export default router;
