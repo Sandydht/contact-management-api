53cb931d365dd2f4b77aaee3d7e803430fee727a
Sandy Dwi Handoko Trapsilo <48269439+Sandydht@users.noreply.github.com>
2024-12-09 01:25:35 +0700

    Delete src/Applications/validator directory

--- src/Applications/validator/UserValidator.ts
+++
@@ -1,9 +0,0 @@
-import { RegisterUser } from "../../Domains/users/entities/RegisterUser";
-
-class UserValidator {
-  async registerUser(payload: RegisterUser): Promise<RegisterUser> {
-    throw new Error('USER_VALIDATOR.METHOD_NOT_IMPLEMENTED');
-  }
-}
-
-export default UserValidator;
